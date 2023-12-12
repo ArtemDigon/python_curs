2fad2ac694450e9017eecde9e4146ec212d686fb
Hacker <artem@MacBook-Air-Artem-2.local>
2023-12-12 11:56:45 +0300

    r and w file

---
+++ lesson_8.py
@@ -0,0 +1,85 @@
+from os.path import exists
+from csv import DictReader, DictWriter
+
+class LenNumberError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+class NameError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+def get_info():
+    print("Введите данные")
+
+    is_valid_name = False
+    while not is_valid_name:
+        try:
+            first_name = input("Имя: ")
+            if len(first_name) < 2:
+                raise NameError("Длина имени должна быть больше 1.")
+            else:
+                is_valid_name = True
+        except NameError as err:
+            print(err)
+            continue
+
+    last_name = input("Фамилия: ")
+
+    is_valid_phone = False
+    while not is_valid_phone:
+        try:
+            phone_number = int(input("Телефон: "))
+            if len(str(phone_number)) != 11:
+                raise LenNumberError("Неверная длина номера.")
+            else:
+                is_valid_phone = True
+        except ValueError:
+            print("Не валидный номер.")
+            continue
+        except LenNumberError as err:
+            print(err)
+            continue
+
+    return [first_name, last_name, phone_number]
+
+def create_file(file_name):
+    with open(file_name, 'w', encoding='utf-8', newline='') as data:
+        f_writer = DictWriter(data, ["Имя", "Фамилия", "Телефон"])
+        f_writer.writeheader()
+
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8', newline='') as data:
+        f_reader = DictReader(data)
+        return list(f_reader)
+
+def write_file(file_name, my_list):
+    res = read_file(file_name)
+    for line in res:
+        if line["Телефон"] == str(my_list[2]):
+            print("Телефон уже записан.")
+            return
+    obj = {'Имя': my_list[0], 'Фамилия': my_list[1], 'Телефон': my_list[2]}
+    res.append(obj)
+    with open(file_name, 'w', encoding='utf-8', newline='') as data:
+        f_writer = DictWriter(data, ["Имя", "Фамилия", "Телефон"])
+        f_writer.writeheader()
+        f_writer.writerows(res)
+
+file_name = 'phone.csv'
+def main():
+    while True:
+        command = input("Введите команду: ")
+        if command == 'q':
+            break
+        elif command == 'w':
+            if not exists(file_name):
+                create_file(file_name)
+            write_file(file_name, get_info())
+        elif command == 'r':
+            if not exists(file_name):
+                print("Файл отсутствует.")
+                continue
+            print(read_file(file_name))
+
+main()
