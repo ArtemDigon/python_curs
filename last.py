658fce7a76c8f8ea52717a2aa53178e46242cfd6
Hacker <artem@MacBook-Air-Artem-2.local>
2023-12-13 20:10:49 +0300

    One hot

--- last.py
+++ last.py
@@ -9,3 +9,10 @@ data = pd.DataFrame({'whoAmI': lst})
 print("Исходный DataFrame:")
 print(data.head())
 
+# One hot
+one_hot_data = pd.get_dummies(data['whoAmI'])
+data_one_hot = pd.concat([data, one_hot_data], axis=1)
+data_one_hot = data_one_hot.drop('whoAmI', axis=1)
+
+print("\nDataFrame после one hot encoding:")
+print(data_one_hot.head())
