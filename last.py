af85e1aa29f82b578eab63a2a78f21fa12beb819
Hacker <artem@MacBook-Air-Artem-2.local>
2023-12-13 20:09:24 +0300

    before

---
+++ last.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Исходный код
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print("Исходный DataFrame:")
+print(data.head())
+
